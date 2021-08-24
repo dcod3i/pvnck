424c09b5a2f29b1dfa6eb2cc4059f2d37e7fd0d4
Pavan Kumar Shetty <pavankumarck.scs20@rvce.edu.in>
2021-08-24 19:48:56 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,3 +10,7 @@ function fullDateTime() {
 setInterval(function () {
     fullDateTime()
 }, 1000);
+
+function myFunction() {
+    document.getElementsByTagName('body')[0].style.cssText = "background: linear-gradient( 90deg, rgb(82, 107, 106) 0%, rgb(255, 255, 255) 100% );"
+}
