4a4ad4a4fb2c8912c1fa5968cad1e84c7d2a0093
Pavan Kumar Shetty <pavankumarck.scs20@rvce.edu.in>
2021-08-24 20:09:13 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ setInterval(function () {
     fullDateTime()
 }, 1000);
 
-function myFunction() {
+function darkMod() {
     document.getElementsByTagName('body')[0].style.cssText = "background: linear-gradient( 90deg, rgb(82, 107, 106) 0%, rgb(255, 255, 255) 100% );"
+}
+
+function dayMod() {
+    document.getElementsByTagName('body')[0].style.cssText = "background: linear-gradient( 90deg, rgba(30, 201, 197, 1) 0%, rgba(250, 255, 138, 1) 100% );"
 }
